97920772036d77c6b952baefcb5b6460cae3963a
Tarek Magdy <tarekmagdy21@outlook.com>
2024-09-04 00:02:01 +0300

    .

--- src/api/user/userCreate.api.ts
+++ src/api/user/userCreate.api.ts
@@ -1,7 +1,7 @@
 import { userCreateType } from "@/types";
 export const userCreate = async (userDetails: userCreateType) => {
   try {
-    const response = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/user`, {
+    const response = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}user`, {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
--- src/api/user/userRegister.api.ts
+++ src/api/user/userRegister.api.ts
@@ -1,7 +1,7 @@
 import { authRegisterType } from "@/types";
 export const userRegister = async (userDetails: authRegisterType) => {
   try {
-    const response = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/user`, {
+    const response = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}user`, {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
